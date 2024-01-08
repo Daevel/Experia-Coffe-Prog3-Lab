d2a6ebeb7649320339e388f90c0c8b2c8e20e555
Luigi Avitabile <luigi.avitabile5@gmail.com>
2024-01-08 22:46:54 +0100

    First commit about Experia Project for Prog3/Lab
    Using Scene builder and Intellij IDEA

---
+++ src/main/java/experia/coffee/experiacoffee/HelloController.java
@@ -0,0 +1,14 @@
+package experia.coffee.experiacoffee;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module experia.coffee.experiacoffee {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    //requires eu.hansolo.tilesfx;
+
+    opens experia.coffee.experiacoffee to javafx.fxml;
+    exports experia.coffee.experiacoffee;
+}
